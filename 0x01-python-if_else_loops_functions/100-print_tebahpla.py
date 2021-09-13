04a4a8c8bed2adc1fc62d3c248d516db06cfe804
knia10 <kelly.stephania@hotmail.com>
2021-09-13 00:55:32 -0500

    Add file 100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,5 +2,7 @@
 if __name__ == '__main__':
     for alp in range(ord('z'), ord('a')-1, -1):
         if alp % 2 != 0:
-            alp = alp - 32
-        print('{}'.format(chr(alp)), end="")
+            alp = (chr(alp - ord('a') + ord('A')))
+        else:
+            alp = chr(alp)
+        print('{}'.format(alp), end="")

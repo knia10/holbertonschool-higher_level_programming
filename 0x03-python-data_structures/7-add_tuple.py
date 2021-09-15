1727f93b148b1982365d2903a3be8d863818b0e9
knia10 <kelly.stephania@hotmail.com>
2021-09-14 22:40:03 -0500

    Add file 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,5 +3,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
     lta = len(tuple_a)
     ltb = len(tuple_b)
     sumt = ((tuple_a[0] if lta > 0 else 0) + (tuple_b[0] if ltb > 0 else 0),
-            (tuple_a[1] if lta > 0 else 0) + (tuple_b[1] if ltb > 1 else 0))
+            (tuple_a[1] if lta > 1 else 0) + (tuple_b[1] if ltb > 1 else 0))
     return sumt

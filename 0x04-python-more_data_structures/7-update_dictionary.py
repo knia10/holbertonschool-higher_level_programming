dd1c6287a5f55dc8150df0843eea0c1150d9be8d
knia10 <kelly.stephania@hotmail.com>
2021-09-15 22:01:38 -0500

    Add file 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if a_dictionary:
-        change_dic = {key: value}
-        a_dictionary.update(change_dic)
-        return(a_dictionary)
-    else:
-        return None
+    change_dic = {key: value}
+    a_dictionary.update(change_dic)
+    return(a_dictionary)

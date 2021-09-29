80b870caae76c3ff81c9dccd80e2e84f42db77a7
knia10 <kelly.stephania@hotmail.com>
2021-09-29 00:16:49 -0500

    Add file 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -6,22 +6,23 @@ class Square:
     '''Define initial state of attribute'''
 
     def __init__(self, size=0):
+        '''inicialiced square'''
         self.size = size
 
-        @property
-        def size(self):
-            '''Getter'''
-            return self.__size
+    @property
+    def size(self):
+        '''Getter'''
+        return self.__size
 
-        @size.setter
-        def size(self, value):
-            '''Setter'''
-            if type(size) != int:
-                raise TypeError('size must be an integer')
-            elif size < 0:
-                raise ValueError('size must be >= 0')
-            else:
-                self.__size = size
+    @size.setter
+    def size(self, value):
+        '''Setter'''
+        if type(value) != int:
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value
 
     '''Public instance method'''
 

5dd0e6214fd978cda1b2aca99cfe62aa24dc666e
knia10 <kelly.stephania@hotmail.com>
2021-10-11 18:40:38 -0500

    Add files and testings

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+Module 0-lookup
+Has a function that returns the list of available attributes
+and methods of an object
+'''
+
+
+
+
+def lookup(obj):
+    '''
+    function that returns the list of available attributes and methods of an object
+    '''
+    return(dir(obj))
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+Module my_list
+Has a class MyList that inherits from list
+'''
+
+
+class MyList(list):
+    '''
+    class that inherits from list
+    '''
+
+    def print_sorted(self):
+        '''
+        method that prints the list, but ascending sort
+        '''
+        print(sorted(self))# sorted retorna newlist

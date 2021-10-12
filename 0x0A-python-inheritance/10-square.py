e3c0d2d477bd80c56a41c2a827ccb51d29ec1e44
knia10 <kelly.stephania@hotmail.com>
2021-10-11 23:42:03 -0500

    import and function

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''
+Module 10-square
+
+extend to BaseGeometry
+contain subclass Rectangle and square
+with private size.
+'''
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    '''
+    inherits from Rectangle and BaseGeometry
+    '''
+    def __init__(self, size):
+        '''
+        Method inicialized with private size
+        '''
+        self.integer_validator('size', size)
+        super().__init__(size,size)
+        self.__size = size

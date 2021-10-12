18702792d6c9a4a175a050fccb41ed11e560940c
knia10 <kelly.stephania@hotmail.com>
2021-10-11 22:47:44 -0500

    import and fucntions

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''
+Module 8-rectangle
+Extend to BaseGeometry
+with subclass Rectangle with privates attributes width, height
+'''
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+
+    def __init__(self, width, height):
+        '''
+        width and height must be positive integers,
+        validated by integer_validator
+        '''
+        # super() trae todo del padre
+        super().integer_validator('width', width)
+        self.__width = width
+        super().integer_validator('height', width)
+        self.__height = height

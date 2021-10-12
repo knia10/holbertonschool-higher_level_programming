6d72d4c10eac138aa6278c05dc68eaa016e1e05d
knia10 <kelly.stephania@hotmail.com>
2021-10-11 19:30:37 -0500

    Add fucntion

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''
+Module inherits_from
+Has a function that returns True if the object is an instance of a class that inherited
+(directly or indirectly) from the specified class ; otherwise False.
+'''
+
+
+def inherits_from(obj, a_class):
+    '''
+    function that returns True if the obj is an subclass
+    otherwise return False.
+    '''
+    if type(obj) is not a_class and issubclass(
+            type(obj), a_class):  # directo type, indirecto sub
+        return True
+    else:
+        return False

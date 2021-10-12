5b77016608f92aad72b2bc30b1a1cc014ada8484
knia10 <kelly.stephania@hotmail.com>
2021-10-11 23:19:33 -0500

    changes isinstance

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -21,9 +21,9 @@ class BaseGeometry:
         '''
         Method that validates value
         '''
-        if not isinstance(value, int):
-            raise TypeError('{:s} must be an integer'.format(name))
         if isinstance(value, bool):
+            raise TypeError('{:s} must be an integer'.format(name))
+        if not isinstance(value, int):
             raise TypeError('{:s} must be an integer'.format(name))
         if value <= 0:
             raise ValueError('{:s} must be greater than 0'.format(name))

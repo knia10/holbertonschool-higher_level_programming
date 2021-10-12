3a10de829cf2fc3289e37d224149bebd556e08b2
knia10 <kelly.stephania@hotmail.com>
2021-10-12 17:43:20 -0500

    Add file 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+Module 3-to_json_string
+function that
+returns the JSON representation of an object (string)
+'''
+
+
+import json
+
+
+def to_json_string(my_obj):
+    '''
+function returns the JSON representation of an object
+    '''
+
+    return json.dumps(my_obj)

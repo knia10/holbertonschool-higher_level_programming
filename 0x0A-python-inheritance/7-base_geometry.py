0cdb9707d1becf22a40b313788e5cf6e37d3f1ca
knia10 <kelly.stephania@hotmail.com>
2021-10-11 21:52:37 -0500

    Add function and test

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''
+Module base_geometry
+Has Public instance method with exception
+'''
+
+
+class BaseGeometry:
+    '''
+    with Public instance method area()
+    public instance method integer_validator(self, name, value)
+    '''
+
+    def area(self):
+        raise Exception('area() is not implemented')
+
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError('{:s} must be an integer'.format(name))
+        if value <= 0:
+            raise ValueError('{:s} must be greater than 0'.format(name))

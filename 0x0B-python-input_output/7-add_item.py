27ef0a32f9eeb2cf731c39c5aeba2939999453fc
knia10 <kelly.stephania@hotmail.com>
2021-10-12 21:30:19 -0500

    Add file 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''
+Module 7-add_item
+Has a script that adds all arguments to a Python list,
+and then save them to a file
+import save_to_json_file from 5-save_to_json_file.py
+import load_from_json_file from 6-load_from_json_file.py
+'''
+from sys import argv
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+# l_fname = 'add_item.json'
+try:
+    file_found = load_from_json_file('add_item.json')
+except FileNotFoundError:
+        file_found = []
+for i in argv[1:]:
+    file_found.append(i)
+save_to_json_file(file_found, 'add_item.json')

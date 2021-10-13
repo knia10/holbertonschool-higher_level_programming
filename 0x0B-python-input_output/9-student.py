3d3ce99bdb8b30cab0f27906f2f8dd0abd8fbcb1
knia10 <kelly.stephania@hotmail.com>
2021-10-12 22:41:27 -0500

    Add file 9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''
+Module 9-student
+Has a class Student
+istantiation with self, first_name, last_name, age
+the class contains Public method def to_json(self)
+'''
+
+
+class Student():
+    '''
+    class Student that defines a student by
+    '''
+
+
+def __init__(self, first_name, last_name, age):
+    '''Instantiation with first_name, last_name and age'''
+    self.first_name = first_name
+    self.last_name = last_name
+    self.age = age
+
+
+def to_json(self):
+    '''
+    retrieves a dictionary representation of a Student instance
+    '''
+    return self.__dict__

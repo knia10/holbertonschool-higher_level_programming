1f21bfe8bf1c5b000478df19082607ce45c51d7b
knia10 <kelly.stephania@hotmail.com>
2021-10-12 22:42:53 -0500

    identend

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -13,15 +13,15 @@ class Student():
     '''
 
 
-def __init__(self, first_name, last_name, age):
-    '''Instantiation with first_name, last_name and age'''
-    self.first_name = first_name
-    self.last_name = last_name
-    self.age = age
+    def __init__(self, first_name, last_name, age):
+        '''Instantiation with first_name, last_name and age'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
 
-def to_json(self):
-    '''
-    retrieves a dictionary representation of a Student instance
-    '''
-    return self.__dict__
+    def to_json(self):
+        '''
+        retrieves a dictionary representation of a Student instance
+        '''
+        return self.__dict__

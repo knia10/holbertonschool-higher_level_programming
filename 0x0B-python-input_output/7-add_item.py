e8564a94cff219c1b7c4feff1d8ab0065f64af77
knia10 <kelly.stephania@hotmail.com>
2021-10-12 23:40:23 -0500

    Add coments

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -13,10 +13,14 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 l_fname = 'add_item.json'
+'''
+The list must be saved
+as a JSON representation in a file named
+'''
 try:
     file_found = load_from_json_file(l_fname)
 except FileNotFoundError:
-    file_found = []
+        file_found = []
 for i in argv[1:]:
     file_found.append(i)
 save_to_json_file(file_found, l_fname)

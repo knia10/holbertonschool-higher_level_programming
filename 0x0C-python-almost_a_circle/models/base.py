02e5810e98cc81922318009ba8f5e744651d5ac6
knia10 <kelly.stephania@hotmail.com>
2021-10-21 00:17:58 -0500

    json dumps

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,7 +4,7 @@ Module base
 Has a Class Base with private class attribute __nb_objects
 class constructor __init__(self, id=None)
 '''
-
+import json
 
 class Base:
     '''
@@ -22,3 +22,13 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''
+        returns the JSON string representation of list_dictionaries
+        '''
+        if list_dictionaries is None or list_dictionaries[0] == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)

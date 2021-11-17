6088f9d121042eb59fa1610c102a4b6ff896a17f
root <root@MED-PF1NQ8WR.hbtn.io>
2021-11-16 23:49:19 -0500

    Update ,

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,7 @@
 -- script that creates a table second_table in the database
 CREATE TABLE IF NOT EXISTS second_table (
-    id INT
-    name VARCHAR(256)
+    id INT,
+    name VARCHAR(256),
     score INT
 );
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES

31598ddd31e34c76fc815987c7f93ed22e55e1b4
knia10 <kelly.stephania@hotmail.com>
2021-11-17 21:49:11 -0500

    Add directory and files

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,3 @@
+-- script that lists all the cities of California that can be found in the database
+SELECT id,name FROM cities
+WHERE state_id =(SELECT id FROM states WHERE name = "California") GROUP BY id ORDER BY id ASC;

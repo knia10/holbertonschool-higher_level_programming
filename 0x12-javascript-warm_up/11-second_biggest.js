899de4268639c595109d07a838e6bc7f1f38c6a3
knia10 <kelly.stephania@hotmail.com>
2021-12-14 00:33:10 -0500

    Add file 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const myVar = process.argv.length;
+const order = process.argv.sort();
+let x = 0;
+if (myVar <= 3) {
+  console.log(0);
+} else {
+  while (x !== myVar) {
+    x++;
+  }
+  x -= 1;
+  console.log(order[x - 1]);
+}

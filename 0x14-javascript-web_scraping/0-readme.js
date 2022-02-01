4968350e4c60345e489bac300720ca9d8b9080e1
knia10 <kelly.stephania@hotmail.com>
2022-02-01 16:42:23 -0500

    Add file 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// script that reads and prints the content of a file.
+
+const file = process.argv[2];
+const fs = require('fs');
+fs.readFile(file, 'utf8', function (err, data) {
+  if (data) {
+    console.log(data);
+  } else {
+    console.log(err);
+  }
+});

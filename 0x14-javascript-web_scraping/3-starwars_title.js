5f8818443f8cb7f1909f3d56da1c02b7b1236c60
knia10 <kelly.stephania@hotmail.com>
2022-02-01 18:23:03 -0500

    Add file 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// script that prints the title of a Star Wars movie where the episode number matches a given integer.
+
+const request = require('request');
+
+const movieId = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + movieId;
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});

2844d5c5effe6b010e50cc26b8ee29a5ed772cce
knia10 <kelly.stephania@hotmail.com>
2022-02-01 16:55:24 -0500

    Modified file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,8 +5,8 @@ const file = process.argv[2];
 const fs = require('fs');
 fs.readFile(file, 'utf8', function (err, data) {
   if (data) {
-    console.log(data);
-  } else {
     console.log(err);
+  } else {
+    console.log(data);
   }
 });

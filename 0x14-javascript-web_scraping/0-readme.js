f9dd5e18ebd10d77bffcca2c1c10e779961b51d5
knia10 <kelly.stephania@hotmail.com>
2022-02-01 17:01:37 -0500

    Modified file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-// script that reads and prints the content of a file.
+/*script that reads and prints the content of a file.*/
 
 const file = process.argv[2];
 const fs = require('fs');
 fs.readFile(file, 'utf-8', function (err, data) {
-  if (data) {
-    console.log(data);
-  } else {
+  if (err) {
     console.log(err);
+  } else {
+    console.log(data);
   }
 });
